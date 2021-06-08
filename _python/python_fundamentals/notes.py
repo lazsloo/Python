3ea81a3e93aca9d8cbb9bbe364990e8fa0621afa
lazsloo <trtaylor2024@gmail.com>
2021-06-07 23:27:44 -0700

    first commit

---
+++ _python/python_fundamentals/notes.py
@@ -0,0 +1,149 @@
+# Data Types:
+
+# List
+empty_list = []
+ninjas = ['Rozen', 'KB', 'Oliver']
+print(ninjas[2]) 	# output: Oliver
+ninjas[0] = 'Francis'
+ninjas.append('Michael')
+print(ninjas)	# output: ['Francis', 'KB', 'Oliver', 'Michael']
+ninjas.pop()
+print(ninjas)	# output: ['Francis', 'KB', 'Oliver']
+ninjas.pop(1)
+print(ninjas)	# output: ['Francis', 'Oliver']
+
+# Dictionary
+empty_dict = {}
+new_person = {'name': 'John', 'age': 38, 'weight': 160.2, 'has_glasses': False}
+new_person['name'] = 'Jack'	# updates if the key exists
+new_person['hobbies'] = ['climbing', 'coding']	# adds a key-value pair if the key doesn't exist
+print(new_person)	
+# output: {'name': 'Jack', 'age': 38, 'weight': 160.2, 'has_glasses': False, 'hobbies': ['climbing', 'coding']}
+w = new_person.pop('weight')	# removes the specified key and returns the value
+print(w)		# output: 160.2
+print(new_person)	
+# output: {'name': 'Jack', 'age': 38, 'has_glasses': False, 'hobbies': ['climbing', 'coding']}
+
+# Type of
+print(type(2.63))		# output: <class 'float'>
+print(type(new_person))		# output: <class 'dict'>
+
+# Length
+print(len(new_person))		# output: 4 (the number of key-value pairs)
+print(len('Coding Dojo'))	# output: 11
+
+# Int -> Str
+# print("Hello" + 42)			# output: TypeError
+print("Hello " + str(42))		# output: Hello 42
+
+# Str -> Int
+total = 35
+user_val = "26"
+# total = total + user_val		# output: TypeError
+total = total + int(user_val)		# total will be 61
+print(total)
+
+# Conditional Statements:
+
+# If, elif , else
+x = 12
+if x > 50:
+    print("bigger than 50")
+else:
+    print("smaller than 50")
+# because x is not greater than 50, the second print statement is the only one that will execute
+
+x = 55
+if x > 10:
+    print("bigger than 10")
+elif x > 50:
+    print("bigger than 50")
+else:
+    print("smaller than 10")
+# even though x is greater than 10 and 50, the first true statement is the only one that will execute, so we will only see 'bigger than 10'
+
+if x < 10:
+    print("smaller than 10")
+# nothing happens, because the statement is false   
+
+# Loops:
+for x in range(0, 10, 2):
+    print(x)
+# output: 0, 2, 4, 6, 8
+for x in range(5, 1, -3):
+    print(x)
+# output: 5, 2
+
+# For loops list
+my_list = ["abc", 123, "xyz"]
+for i in range(0, len(my_list)):
+    print(i, my_list[i])
+# output: 0 abc, 1 123, 2 xyz
+
+# OR 
+
+for v in my_list:
+    print(v)
+# output: abc, 123, xyz
+
+# For loops dictionary
+my_dict = { "name": "Noelle", "language": "Python" }
+for k in my_dict:
+    print(k)
+# output: name, language
+
+my_dict = { "name": "Noelle", "language": "Python" }
+for k in my_dict:
+    print(my_dict[k])
+# output: Noelle, Python
+
+# another way to iterate through the keys
+# for key in capitals.keys():
+#     print(key)
+
+#to iterate through the values
+# for val in capitals.values():
+#     print(val)
+
+#to iterate through both keys and values
+# for key, val in capitals.items():
+#     print(key, " = ", val)
+
+# While loops
+count = 0
+while count < 5:
+    print("looping - ", count)
+    count += 1
+
+# Loops else
+y = 3
+while y > 0:
+    print(y)
+    y -= 1
+else:
+    print("Final else statement")
+
+# Loop break
+for val in "string":
+    if val == "i":
+        break
+    print(val)
+# output: s, t, r
+
+# Loop continue
+for val in "string":
+    if val == "i":
+        continue
+    print(val)
+# output: s, t, r, n, g
+# notice, no i in the output, but the loop continued after the i
+
+y = 3
+while y > 0:
+    print(y)
+    y = y - 1
+    if y == 0:
+        break
+else:		# only executes on a clean exit from the while loop (i.e. not a break)
+    print("Final else statement")
+# output: 3, 2, 1
