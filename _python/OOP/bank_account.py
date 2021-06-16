62b60e1cbd73d3bd8fa7a559514f8082f982df4d
lazsloo <trtaylor2024@gmail.com>
2021-06-16 01:37:24 -0700

    first commit

--- _python/OOP/bank_account.py
+++ _python/OOP/bank_account.py
@@ -21,8 +21,12 @@ class BankAccount:
         return self
 
     def yield_interest(self):
-
+        amount = int(self.rate * self.bal)
+        self.bal = amount
         return self
 
-bank = BankAccount(0.01, 0)
-bank.deposit(100).display_account_info()
+bank_of_fail = BankAccount(0.01, 0)
+bank_of_fail.deposit(200).deposit(150).deposit(50).withdraw(100).yield_interest().display_account_info()
+
+bank_of_love = BankAccount(0.01, 0)
+bank_of_love.deposit(1500).deposit(1500).withdraw(200).withdraw(150).withdraw(300).withdraw(50).yield_interest().display_account_info()
