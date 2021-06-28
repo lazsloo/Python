e3c1fd766791a872626fe4af87750c38b0afdcf5
lazsloo <trtaylor2024@gmail.com>
2021-06-27 22:46:28 -0700

    commit

--- django/dojo_survey/dojo_survey/dojo_survey_app/urls.py
+++ django/dojo_survey/dojo_survey/dojo_survey_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('create_user', views.create_user),
+    path('muthafucka/', views.muthafucka),
 ]
--- django/dojo_survey/dojo_survey/dojo_survey_app/views.py
+++ django/dojo_survey/dojo_survey/dojo_survey_app/views.py
@@ -1,14 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
 def index(request):
     return render(request, "index.html")
 
 def create_user(request):
-    context = {
-        "first_name": request.POST["first_name"],
-        "locations": request.POST["locations"],
-        "language": request.POST["language"],
-        "comment": request.POST["comment"]
-    }
-    return render(request, "results.html", context)
+    print("Getting info from this rat bastard"),
+    request.session['first_name'] = request.POST['first_name']
+    request.session['locations'] = request.POST['locations']
+    request.session['language'] = request.POST['language']
+    request.session['comment'] = request.POST['comment']
+    # context = {
+    #     "first_name": request.POST["first_name"],
+    #     "locations": request.POST["locations"],
+    #     "language": request.POST["language"],
+    #     "comment": request.POST["comment"]
+    # }
+    return redirect('muthafucka/')
+
+def muthafucka(request):
+    print("Got that bastahds info")
+    return render(request, "results.html")
