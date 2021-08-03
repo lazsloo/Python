4573cc02f1d3fd82679a7efdc8aaa6618459c366
lazsloo <trtaylor2024@gmail.com>
2021-08-02 21:58:43 -0700

    ignore secret key test

--- projects/first_proj/first_proj/settings.py
+++ projects/first_proj/first_proj/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from . import secret
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!5$n#n0!t$59&l*lqo#7zykaxu5mhcr9c)zma1b6%yui&=86mm'
+SECRET_KEY = secret.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
